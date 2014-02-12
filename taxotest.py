262acfb71a8a9d52dd7c2c8897bd69b2fbe47cef
Diana Vermilya <dianavermilya@gmail.com>
2014-02-11 21:22:27 -0500

    scatter up

--- taxotest.py
+++ taxotest.py
@@ -8,66 +8,67 @@ import codecs
 import datetime
 
 def reviseDataType (string):
-	try:
-		return int(string)
-	except:
-		pass
-	try:
-		return float(string)
-	except:
-		pass
-	if string.strip() == "":
-		return None
-	if len(string.split("/")) == 3:
-		[day, month, year] = string.split("/")
-		try:
-			return datetime.date(int(year), int(month), int(day))
-		except:
-			pass
+    try:
+        return int(string)
+    except:
+        pass
+    try:
+        return float(string)
+    except:
+        pass
+    if string.strip() == "":
+        return None
+    if len(string.split("/")) == 3:
+        [day, month, year] = string.split("/")
+        try:
+            return datetime.date(int(year), int(month), int(day))
+        except:
+            pass
 
-	return string
+    return string
 
 
 
 def dataToDict(filename):
-	d = {}
-	f = open(filename, 'r')
-	wr = open("strings" + filename, "w")
-	count = 0
-	
-	# strip line ending characters
-	keyString = f.readline().rstrip()
-	
-	# check for and strip utf-8 BOM (python bug wont auto-remove it :( )
-	if keyString[0:3] == codecs.BOM_UTF8:
-	    keyString = keyString[3:]
-	  
-	keys = keyString.split(",")
-	
-	for key in keys:
-		d[key] = []
-	for line in f:
-		ans=line.split(",")
-		for i in range(len(keys)):
-			val = reviseDataType(ans[i])
-			if isinstance(val, str):
-				count += 1
-				wr.write(val + "\n--\n")
-			d[keys[i]].append(val)
-	print "count: ", count
-	
-	return d
+    d = {}
+    tagToDataType = {}
+    f = open(filename, 'r')
+    wr = open("strings" + filename, "w")
+    count = 0
+    
+    # strip line ending characters
+    keyString = f.readline().rstrip()
+    
+    # check for and strip utf-8 BOM (python bug wont auto-remove it :( )
+    if keyString[0:3] == codecs.BOM_UTF8:
+        keyString = keyString[3:]
+      
+    keys = keyString.split(",")
+    
+    for key in keys:
+        d[key] = []
+    for line in f:
+        ans=line.split(",")
+        for i in range(len(keys)):
+            val = reviseDataType(ans[i])
+            if isinstance(val, str):
+                count += 1
+                wr.write(val + "\n--\n")
+            d[keys[i]].append(val)
+    print "count: ", count
+    
+    return d
 
 
 def mean(d, key):
     vals = d[key]
     s = 0
     try:
-	    for val in vals:
-	        s += int(val)
-	    return float(s)/len(val)
-	except:
-		"One of your values is not a number."
+        for val in vals:
+            s += int(val)
+        return float(s)/len(val)
+    except:
+        "One of your values is not a number."
 
 def AgePmf(d):
     pmf = thinkstats2.MakePmfFromList(d['age'], 'age')
@@ -86,8 +87,17 @@ def AgeCdf(d):
                xlabel='age(years)',
                ylabel='probability')
 
+def Scatter(d, var1, var2):
+    """Assuming numerical for now (will call isNumerical()"""
+    xs = d[var1]
+    ys = d[var2]
+    thinkplot.Scatter(xs, ys)
+    thinkplot.show()
 
 beths = dataToDict('beths.csv')
 taxo = dataToDict('taxo.csv')
+
+Scatter(taxo, 'ComsxFac', 'anxwom')
+
 #AgeCdf(beths)
 #AgeCdf(taxo)
